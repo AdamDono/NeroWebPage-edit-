61ec1795d6325f72e6f044b1a2a837915285727c
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-08-22 12:15:54 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { LandingComponent } from './landing/landing.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {path: 'landing', component: LandingComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
