37784c945be298335089213db9ac259233b61fda
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-09-12 15:13:54 +0200

    signup validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +10,7 @@ import { PurchaseComponent } from './purchase/purchase.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -21,11 +23,15 @@ import { LoginComponent } from './login/login.component';
     MarketplaceComponent,
     RegisterComponent,
     LoginComponent,
-
+  
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,RouterModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
+import { FormsModule } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
+  registerForm:any;
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      "FirstName": new FormControl (null, [Validators.required,Validators.pattern('[a-zA-Z]*')]),
+      "LastName": new FormControl (null,[Validators.required,Validators.pattern('[a-zA-Z]*')]),
+      "Email": new FormControl (null,[Validators.required,Validators.email]),
+      "Phone": new FormControl (null,[Validators.required,Validators.pattern('[0-9]*')])
+    }); 
   }
+
+  //submit funtion
+  submitData()
+{
+  console.log(this.registerForm );
+
+  if (this.registerForm.valid)
+  { 
+
+alert(`Thank You ${this.registerForm.value.FirstName}`)
+
+
+  }
+
+
+  
+}
+
+get FirstName() {return this.registerForm.get('FirstName');}
+get LastName() {return this.registerForm.get('LastName');}
+get Email() {return this.registerForm.get('Email');}
+get Phone() {return this.registerForm.get('Phone');}
 
 }
